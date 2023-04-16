0ff70c6263b666b924103ecafc4b42ee964523b1
Anas kc <anaskc@Anass-MacBook-Air.local>
2023-04-16 06:16:06 +0400

    deployed 1.3

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom'
 
 import App from './App'
 import  './index.css'
-<script src="https://unpkg.com/react-router-dom/umd/react-router-dom.min.js"></script>
+
 
 ReactDOM.render(<App/>,document.getElementById('root'))
